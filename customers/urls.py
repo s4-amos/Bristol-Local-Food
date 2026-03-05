ff2c6c0633f992c7f2017c4a69c245fe29d40617
Rajib Ale <75166902+Alerajib31@users.noreply.github.com>
2026-03-05 19:13:05 +0000

    Revert "Sprint 1 (#3)"
    This reverts commit 9a7e0b3453030787076fffeb67a4de0c1a73fe72.

--- customers/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-URLs for the customers app – currently only registration.
-"""
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.customer_register, name='customer_register'),
-]
--- static/js/order.js
+++
@@ -1,1 +0,0 @@
-// Placeholder for future app‑specific JavaScript
